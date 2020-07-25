26a07fc35ab4a1eaadfe68d99cd6755476b930a3
ashok-sahu <ashoksahu1105@gmail.com>
2020-07-26 01:09:19 +0530

    completed

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require("express");
-const chalk = require('chalk')
+const chalk = require("chalk");
 const cors = require("cors");
-const connectDB = require('./config/db')
+const path = require("path");
+const connectDB = require("./config/db");
 
-const app = express()
-require('dotenv').config()
+const app = express();
+require("dotenv").config();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 //middleware connection
-app.use(cors())
-app.use(express.json({ extended: true }))
+app.use(cors());
+app.use(express.json({ extended: true }));
 
 //database(mongodb) connection
-connectDB()
+connectDB();
 
 //routes
-app.use('/articles',require('./routes/Articles'))
+app.use("/articles", require("./routes/Articles"));
+
+app.use(express.static(path.join(__dirname, "web", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "web", "build", "index.html"));
+});
 
 //server connection
-app.listen(PORT,()=>{
-    console.log(chalk.bgMagenta.bold.white(`server is running in http://localhost:${PORT}`))
-})
+app.listen(PORT, () => {
+  console.log(
+    chalk.bgMagenta.bold.white(`server is running in http://localhost:${PORT}`)
+  );
+});
